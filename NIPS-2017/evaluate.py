9400dc938926cb828908787aca2b6a6693f31e40
Roy-L <yl490@duke.edu>
2019-01-10 15:28:31 -0500

    Clean code

--- NIPS-2017/evaluate.py
+++ NIPS-2017/evaluate.py
@@ -1,22 +1,19 @@
 import os
 import numpy as np
 import pandas as pd
+import csv
 from PIL import Image, ImageFilter
 from scipy.misc import imread
 import imageio
 import tensorflow as tf
 from tensorflow.contrib.slim.nets import inception
-import scipy.io as sio
 from image_utils import load_images, load_imagefiles, jpeg, l2difference, sharpen, smoothen
 from model_utils import InceptionModel, print_accuracy, reconstruct_image
-from patch_vae import patch_vae_model
-from vae_clip import patch_vae_model_64
-from patch_vae_16 import patch_vae_model_16
+from vae_model import patch_vae_model
 from io import BytesIO
 from sklearn.feature_extraction.image import extract_patches_2d
 import keras
 from cleverhans.attacks import FastGradientMethod
-import csv
 from keras import backend as K
 import matplotlib.pyplot as plt
 
@@ -29,12 +26,13 @@ num_classes = 1001
 image_labels = pd.read_csv("./image_info/images.csv")
 attack_epsilon = 0.005
 patch_size = 32
+adversarial_input_path = 'fgsm_images_'
+result_file = 'fgsm_results_163264.csv'
 
 keras.backend.clear_session()
 tf.reset_default_graph()
 
 images = next(load_imagefiles('./images/', batch_shape))
-# non_targeted_images = next(load_imagefiles('./nontargeted_images_{}'.format(attack_epsilon), batch_shape))
 originalimages = (images[1] + 1.0) / 2.0
 filenames = images[0]
 true_labels = []
@@ -70,15 +68,13 @@ vae_model_16.add_loss(kl_loss_16)
 vae_model_16.compile(optimizer=AdamOptimizer)
 vae_model_16.load_weights('vae_16.h5')
 
-# vae_images = reconstruct_image(images=((nontargeted_images+1.0)/2.0), model=vae_model,patch_size=64,step_size=64)
-
 epses = [0.005, 0.0135, 0.022, 0.0305, 0.039, 0.0475, 0.056, 0.0645,
          0.073, 0.0815, 0.09]
 results = []
 for i in epses:
-    # nontargeted_images = next(load_images('./nontargeted_images_{}'.format(i), batch_shape, is_training=False))
+
     nontargeted_images = next(
-        load_images('/work/yl490/adversarial_images/nontargeted_images_{}'.format(i), batch_shape, is_training=False))
+        load_images(adversarial_input_path + str(i), batch_shape, is_training=False))
 
     vae_images_1608 = reconstruct_image(images=((nontargeted_images + 1.0) / 2.0), model=vae_model_16, patch_size=16,
                                         step_size=8)
@@ -93,14 +89,7 @@ for i in epses:
                                         step_size=32)
     vae_images_6464 = reconstruct_image(images=((nontargeted_images + 1.0) / 2.0), model=vae_model_64, patch_size=64,
                                         step_size=64)
-    '''
-    vae_images_1608 = sharpen(vae_images_1608)
-    vae_images_1616 = sharpen(vae_images_1616)
-    vae_images_3216 = sharpen(vae_images_3216)
-    vae_images_3232 = sharpen(vae_images_3232)
-    vae_images_6432 = sharpen(vae_images_6432)
-    vae_images_6464 = sharpen(vae_images_6464)
-    '''
+ 
     vae_images_1608 = smoothen(vae_images_1608)
     vae_images_1616 = smoothen(vae_images_1616)
     vae_images_3216 = smoothen(vae_images_3216)
@@ -131,7 +120,7 @@ for i in epses:
               print_accuracy(mix3 * 2.0 - 1.0, true_labels)]
     print(result)
     results.append(result)
-    with open('fgsm_results_163264.csv', 'a') as csvfile:
+    with open(result_file, 'a') as csvfile:
         # print("writing {}".format())
         writer = csv.writer(csvfile)
         writer.writerow(result)
